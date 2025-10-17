6ac54b2c84c3fc84449756d3b87c36ceb023be56
Baumeister09 <niclas.doellinge@gmail.com>
2025-10-17 04:15:33 +0200

    Booleans und Vergleichsoperatoren

---
+++ Funktionen.js
@@ -0,0 +1,29 @@
+//Eine Funktion ist ein Block von Anweisungen, der beliebig oft ausgeführt werden kann
+//Funktionen können Parameter (Eingabewerte) und Rückgabewerte haben
+//Funktionen kann man mit verschiedenen Schlüsselwörtern deklarieren (z.B. "function", "const", "let", "var")
+//Funktionen werden mit geschweiften Klammern {} definiert
+
+//Funktion deklarieren mit dem Schlüsselwort "function"
+function farbe(){             //Funktionsname: farbe
+    return "grün";           //return = Rückgabewert der Funktion. Anweisungen nach return werden nicht mehr ausgeführt
+}
+let ergebnis = farbe();     //Funktion aufrufen und Rückgabewert in Variable speichern
+
+//parameter (Eingabewerte) in Funktionen
+function Obst(eingabe){
+    return eingabe;        
+}
+let ergebnis2 = Obst("Apfel"); //eingabe besitzt jetzt den Wert "Apfel", genau wie die Variable ergebnis2
+//Parameter können beliebig bennant werden außer wie die Funktion selbst
+
+function logge() {
+    console.log("Hello World!"); //gibt "Hello World!" in der Konsole aus
+}
+function logge(wert) {
+    console.log(wert); //gibt den Wert des Parameters in der Konsole aus
+}
+function logge(wert) {
+    let wert = wert + wert;    //Variable wert wird deklariert und besitzt jetzt den verdoppelten Wert des Parameters
+    console.log(wert);        //gibt den verdoppelten Wert in der Konsole aus
+    return wert;             //gibt den verdoppelten Wert als Rückgabewert der Funktion zurück
+}
